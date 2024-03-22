5eb477ae23b5eb325d21caa81cac79e0d5bfe49e
MarkoG111 <markogacanovic111@gmail.com>
2024-03-22 13:14:32 +0100

    bu

--- Domain/obj/Debug/net8.0/domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab0d35348d37cb7645355323c33e0456d1cec62d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+945a59dd8e2813e69aa50def1e2580709a81ed9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
