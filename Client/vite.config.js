e47ad9b754a55db375536343fe6d4ac6b6f69648
MarkoG111 <markogacanovic111@gmail.com>
2024-12-07 16:56:29 +0100

    Optimized Vite configuration

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,29 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+const proxyPaths = [
+  '/posts',
+  '/categories',
+  '/notifications',
+  '/auth',
+  '/users',
+  '/comments',
+  '/images',
+  '/followers',
+  '/authorrequests',
+  '/usecaselogs',
+  '/register',
+  '/login',
+  '/notificationsHub'
+]
+
+const createProxyConfig = (path) => ({
+  target: 'http://localhost:5207',
+  secure: false,
+  ws: path === '/notificationsHub', // Enable WebSocket proxy for notificationsHub only
+})
+
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    proxy: {
-      '/posts': {
-        target: 'http://localhost:5207',
-        secure: false,
-      },
-      '/categories': {
-        target: 'http://localhost:5207',
-        secure: false,
-      },
-      '/notifications': {
-        target: 'http://localhost:5207',
-        secure: false,
-      },
-      '/notificationsHub': {
-        target: 'http://localhost:5207',
-        secure: false,
-        ws: true, // Enable WebSocket proxying
-      },
-    },
+    proxy: Object.fromEntries(proxyPaths.map((path) => [path, createProxyConfig(path)])),
   },
-
   plugins: [react()],
 })
